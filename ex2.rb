e474dc9b017311a51a5026b6e83fbd1b99f0f667
Praaveen Ranganathan <praaveen@eggheadcreative.com>
2013-05-08 12:02:35 +0530

    Testing comments

--- ex2.rb
+++ ex2.rb
@@ -6,6 +6,7 @@ puts "I could have code like this." # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run."
+# Adding more comments
 
 puts "This will run."
 
