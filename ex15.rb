b67c331dac0b20382150c33f4ed41c9b28ee5a4d
Tripura Konda <tripura@eggheadcreative.com>
2013-05-08 16:40:58 +0530

    writing to files.

---
+++ ex15.rb
@@ -0,0 +1,16 @@
+filename = STDIN.gets.chomp()
+
+prompt = "> "
+txt = File.open(filename)
+
+puts "Here's your file: #{filename}"
+puts txt.read()
+
+puts "I'll also ask you to type it again:"
+print prompt
+file_again = STDIN.gets.chomp()
+
+txt_again = File.open(file_again)
+
+puts txt_again.read()
+
